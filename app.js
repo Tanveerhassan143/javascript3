a4c1990de1208648c5e837a5d0a7523296fb94d8
unknown <ranatanveerhassan04@gmail.com>
2023-10-06 20:18:54 +0500

    okis

---
+++ app.js
@@ -0,0 +1,23 @@
+alert("welcome")
+var Subject=prompt("Enter your Subject");
+
+if(Subject == "English"){
+document.write("<h3>English==>" + " "+"Tota=>100" + "  "+ "  " +"Obtained=>55" + "  "+ "  "+"Percentage=>55%</h3>");
+}
+else if(Subject == "Math") {
+    document.write("<h3>English==>" + " "+"Tota=>100" + "  "+ "  " +"Obtained=>74" + "  "+ "  "+"Percentage=>74%</h3>");
+} 
+else if(Subject == "Urdu") {
+    document.write("<h3>English==>" + " "+"Tota=>100" + "  "+ "  " +"Obtained=>51" + "  "+ "  "+"Percentage=>51%</h3>");
+} 
+else if(Subject == "Islamiyat") {
+    document.write("<h3>English==>" + " "+"Tota=>100" + "  "+ "  " +"Obtained=>43" + "  "+ "  "+"Percentage=>43%</h3>");
+} 
+else if(Subject == "Pak Sutdy") {
+    document.write("<h3>English==>" + " "+"Tota=>100" + "  "+ "  " +"Obtained=>33" + "  "+ "  "+"Percentage=>33%</h3>");
+} 
+else{ 
+    document.write("<h4>please reinter correct spalling and correct subject</h4>")
+}
+
+
